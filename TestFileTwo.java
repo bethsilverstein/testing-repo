0bab8cd9d8d6f7ac0b57069a89a987da82eb7158
Beth Silverstein <beth.silverstein@tradeshift.com>
2022-03-21 11:20:09 -0500

    two java test files added  Changes to be committed: 	new file:   TestClass.java 	new file:   TestFileTwo.java  Changes not staged for commit: 	modified:   TestFileTwo.java

---
+++ TestFileTwo.java
@@ -0,0 +1,13 @@
+
+public class TestFileTwo {
+
+	public static void printTest(String s){
+		System.out.println(s);
+	}
+	
+	public static void main(String[] args){
+		System.out.println("What's up sevenup");
+
+	}
+
+}
