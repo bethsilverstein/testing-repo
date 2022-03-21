319dc61ea07baba36964fbd18b418ea13551a3b5
Beth Silverstein <beth.silverstein@tradeshift.com>
2022-03-21 13:16:29 -0500

    added method to print whats up message

--- TestFileMainBranch.java
+++ TestFileMainBranch.java
@@ -1,6 +1,12 @@
 public class TestFileMainBranch{
+	
+	
+	public static void whatsUp(){
+		System.out.println("What's upppp");
+	}
+	
 	public static void main(String[] args){
-		//blank method for now
+		whatsUp();
 	}
 
 }
